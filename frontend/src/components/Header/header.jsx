a347f4156e923574857b71e7ce2a88c052b7860d
Nikhil Saxena <98411087+saxena-nikhil738@users.noreply.github.com>
2024-04-07 22:28:13 +0530

    Update header.jsx

--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -1,46 +1,69 @@
 import react, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { FaLinkedin, FaInstagramSquare } from "react-icons/fa";
 import "./header.css";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { DropdownButton, Dropdown, Button } from "react-bootstrap";
 import { useAuth } from "../../context/auth";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import * as Cookies from "es-cookie";
+import Dialog from "@mui/material/Dialog";
 
 const Header = () => {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
   const [showList, setShowList] = useState(window.innerWidth < 664);
   const [hambed, setHambed] = useState(false);
+  const [logout, setLogout] = useState(false);
   const [auth, setAuth] = useAuth();
   const [dash, setDash] = useState();
   const token = Cookies.get("token");
 
-  const toggle = () => {
-    setHambed(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const eraseData = () => {
     setAuth({
       ...auth,
       username: "",
       enum: "",
       email: "",
     });
+
+    Cookies.remove("token");
+    Cookies.remove("data");
+    localStorage.removeItem("auth");
     toast.error("Logged out!", {
       position: toast.POSITION.TOP_RIGHT,
       className: "toast-message",
       autoClose: 2000,
     });
-
-    Cookies.remove("token");
-    Cookies.remove("data");
-    localStorage.removeItem("auth");
-    // navigate("/");
+    setOpen(false);
+    navigate("/");
   };
+
+  const toggle = () => {
+    setOpen(true);
+    setHambed(false);
+  };
+  useEffect(() => {}, [open]);
 
   return (
     <>
+      <Dialog open={open} onClose={handleClose}>
+        <div className="log-out">
+          <div className="logout-title">Are you sure ?</div>
+          <div className="btn-logout">
+            <button className="sure-log" onClick={eraseData}>
+              Logout
+            </button>
+            <button className="no-log" onClick={() => setOpen(false)}>
+              Cancel
+            </button>
+          </div>
+        </div>
+      </Dialog>
       <nav className="main-nav">
         {/* Logo */}
         <div className="hamb-cont">
